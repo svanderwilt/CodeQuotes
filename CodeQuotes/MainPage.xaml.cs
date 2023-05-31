058439186ae73c7097d47d40ed9cd3bb2c4e51cd
Scott Vanderwilt <scottvanderwilt@Scotts-iMac.local>
2023-05-30 21:41:24 -0700

    Initial project creations

--- CodeQuotes/MainPage.xaml.cs
+++ CodeQuotes/MainPage.xaml.cs
@@ -2,24 +2,68 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    Random random = new Random();
+	List<string> quotes = new List<string>();
 
-	public MainPage()
+    public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    async Task LoadMauiAsset()
+    {
+        using var stream = await FileSystem.OpenAppPackageFileAsync("quotes.txt");
+        using var reader = new StreamReader(stream);
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        while(reader.Peek() != -1)
+		{
+			quotes.Add(reader.ReadLine());
+		}
+    }
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+		await LoadMauiAsset();
+    }
+
+    void btnGenerateQuote_Clicked(System.Object sender, System.EventArgs e)
+    {
+		var startColor =
+			System.Drawing.Color.FromArgb(
+				random.Next(0, 256),
+				random.Next(0, 256),
+				random.Next(0, 256));
+
+		var endColor =
+			System.Drawing.Color.FromArgb(
+				random.Next(0, 256),
+				random.Next(0, 256),
+				random.Next(0, 256));
+
+		var colors =
+			ColorUtility
+				.ColorControls
+				.GetColorGradient(startColor, endColor, 6);
+
+		float stopOffset = .0f;
+		var stops = new GradientStopCollection();
+		foreach(var c in colors)
+		{
+			stops.Add(new GradientStop(Color.FromArgb(c.Name), stopOffset));
+			stopOffset += .2f;
+		}
+
+		var gradient = new LinearGradientBrush(stops, new Point(0, 0), new Point(1, 1));
+
+		background.Background = gradient;
+
+		int index = random.Next(quotes.Count);
+
+		quote.Text = quotes[index];
+
+		
+    }
 }
 
 
